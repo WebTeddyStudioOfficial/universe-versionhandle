577dafce659c1bf417b1717dca47df104816e47f
WebTeddyStudioOfficial <163024257+WebTeddyStudioOfficial@users.noreply.github.com>
2024-05-09 06:46:34 -0600

    Update versionhandleserver.js

--- versionhandleserver.js
+++ versionhandleserver.js
@@ -1,7 +1,7 @@
 let versionhandleconnected = true;
 let serverversion = "1.4";
 
-if (localVersion !== serverversion) {
+if (localversion !== serverversion) {
   ask('Universe is outdated, please update to <b> ' + serverversion + ' </b> click below to download.), function() {
       window.location.href = "webteddystudioofficial.github.io/web-teddy-studio/universe/1-4-1.zip";
   };
